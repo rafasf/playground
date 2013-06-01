0c9b1116a73637019612b5fe7ca42b92fd27900f
Rafael Ferreira <rafaelsf@gmail.com>
2013-06-01 15:58:10 -0400

    Move "definitions" to a source file

--- fp-oo/ch02-barely-object.clj
+++ fp-oo/ch02-barely-object.clj
@@ -1,34 +1,4 @@
-(def Point
-  (fn [x y]
-    {:x x
-     :y y
-     :__class_symbol__ 'Point}))
-
-(def x :x)
-(def y :y)
-(def class :__class_symbol__)
-
-(def shift
-  (fn [this xinc yinc]
-    (Point (+ (:x this) xinc)
-           (+ (:y this) yinc))))
-
-(def Triangle
-  (fn [point point2 point3]
-    { :point1 point :point2 point2 :point3 point3
-      :__class_symbol 'Triangle }))
-
-(def right-triangle (Triangle (Point 0 0)
-                              (Point 0 1)
-                              (Point 1 0)))
-
-(def equal-right-triangle (Triangle (Point 0 0)
-                                    (Point 0 1)
-                                    (Point 1 0)))
-
-(def different-triangle (Triangle (Point 0 0)
-                                  (Point 0 10)
-                                  (Point 10 0)))
+(load-file "src/ch02-barely-object.clj")
 
 ;; Exercise 01
 (def add
