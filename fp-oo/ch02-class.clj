f56ba55ba8f057687195d709dacf8acb3653438d
Rafael Ferreira <rafaelsf@gmail.com>
2013-06-03 23:25:08 -0400

    Ch02 Extract method lookup

--- fp-oo/ch02-class.clj
+++ fp-oo/ch02-class.clj
@@ -1,14 +1,16 @@
+(def apply-message-to
+  (fn [class instance message args]
+    (let [method (message (:__instance_methods__ class))]
+      (apply method instance args))))
+
 (def a
   (fn [class & args]
-    (let [seeded { :__class_symbol__ (:__own_symbol__ class) }
-          constructor (:add-instance-values (:__instance_methods__ class))]
-      (apply constructor seeded args))))
+    (let [seeded { :__class_symbol__ (:__own_symbol__ class) }]
+      (apply-message-to Point seeded :add-instance-values args))))
 
 (def send-to
   (fn [instance message & args]
-    (let [class (eval (:__class_symbol__ instance))
-          method (message (:__instance_methods__ class))]
-      (apply method instance args))))
+    (apply-message-to Point instance message args)))
 
 (def Point
   {
@@ -20,6 +22,6 @@
                            (assoc this :x x :y y))
     :shift (fn [this xinc yinc]
              (a Point (+ (:x this) xinc)
-                    (+ (:y this) yinc)))
+                      (+ (:y this) yinc)))
     }
 })
