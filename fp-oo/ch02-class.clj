738aae799d3418534c8aae7ed009d3fa942d228d
Rafael Ferreira <rafaelsf@gmail.com>
2013-06-04 12:02:49 -0500

    Ch02 Add class-name and class methods

--- fp-oo/ch02-class.clj
+++ fp-oo/ch02-class.clj
@@ -1,23 +1,31 @@
+(def method-of
+  (fn [message class]
+    (message (:__instance_methods__ class))))
+
+(def class-of
+  (fn [instance]
+    (eval (:__class_symbol__ instance))))
+
 (def apply-message-to
   (fn [class instance message args]
-    (let [method (message (:__instance_methods__ class))]
-      (apply method instance args))))
+    (apply (method-of message class) instance args)))
 
 (def a
   (fn [class & args]
     (let [seeded { :__class_symbol__ (:__own_symbol__ class) }]
-      (apply-message-to Point seeded :add-instance-values args))))
+      (apply-message-to class seeded :add-instance-values args))))
 
 (def send-to
   (fn [instance message & args]
-    (apply-message-to Point instance message args)))
+    (apply-message-to (class-of instance) instance message args)))
 
 (def Point
   {
    :__own_symbol__ 'Point
    :__instance_methods__
    {
-    :class :__class_symbol__
+    :class-name :__class_symbol__
+    :class (fn [this] (class-of this))
     :add-instance-values (fn [this x y]
                            (assoc this :x x :y y))
     :shift (fn [this xinc yinc]
