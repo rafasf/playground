490e55b1ff02c3e070e32cf78a48cb848c0d7c0b
Rafael Ferreira <rafaelsf@gmail.com>
2013-06-10 07:22:54 -0500

    Sum sequence of numbers

--- fp-oo/ch02-recursion.clj
+++ fp-oo/ch02-recursion.clj
@@ -11,3 +11,10 @@
     (if (= 1 n)
       so-far
       (factorial-2 (dec n) (* n so-far)))))
+
+; Exercise 3
+(def add-numbers
+  (fn [numbers so-far]
+    (if (empty? numbers)
+      so-far
+      (add-numbers (rest numbers) (+ (first numbers) so-far)))))
