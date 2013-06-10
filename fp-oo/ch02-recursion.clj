d9ace68c5447b604af6232cfa8328c3560208084
Rafael Ferreira <rafaelsf@gmail.com>
2013-06-10 07:57:28 -0500

    Multiply sequence of numbers

--- fp-oo/ch02-recursion.clj
+++ fp-oo/ch02-recursion.clj
@@ -18,3 +18,10 @@
     (if (empty? numbers)
       so-far
       (add-numbers (rest numbers) (+ (first numbers) so-far)))))
+
+; Exercise 4
+(def multiply-numbers
+  (fn [numbers so-far]
+    (if (empty? numbers)
+      so-far
+      (multiply-numbers (rest numbers) (* (first numbers) so-far)))))
