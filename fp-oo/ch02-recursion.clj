d39ef194f0f8a3ef47327b83ba477d90a1bce845
Rafael Ferreira <rafaelsf@gmail.com>
2013-06-10 07:18:04 -0500

    Calculate factorial using the second pattern

--- fp-oo/ch02-recursion.clj
+++ fp-oo/ch02-recursion.clj
@@ -4,3 +4,10 @@
     (if (= 1 n)
       n
       (* n (factorial (dec n))))))
+
+; Exercise 2
+(def factorial-2
+  (fn [n so-far]
+    (if (= 1 n)
+      so-far
+      (factorial-2 (dec n) (* n so-far)))))
