0f869819e35973b7da9b81d829244132801fdd04
Rafael Ferreira <rafaelsf@gmail.com>
2013-06-13 18:26:19 -0500

    Validate if registrant has the necessary pre-requisites

--- fp-oo/ch03-scheduling.clj
+++ fp-oo/ch03-scheduling.clj
@@ -1,5 +1,9 @@
 (load-file "src/ch03-scheduling.clj")
 
+; Exercise 01
+;; Sample data
+;; [{ :course-name "zig" :morning? true :limit 4 :registered 0 } { :course-name "zag" :morning false :limit 4 :registered 0 } ])
+;; { :manager? true }
 (def note-unavailability
   (fn [courses instructor-count manager?]
     (let [out-of-instructors?
@@ -22,3 +26,31 @@
       domain-annotations
       (note-unavailability instructor-count (:manager? registrants-courses)))))
 
+
+; Exercise 02
+;; Sample data
+;; [{ :course-name "zig" :morning? true :limit 4 :registered 0 :prerequisite ["zag"] } { :course-name "zag" :morning false :limit 4 :registered 0 :prerequisite ["another"] } ])
+;; { :manager? true :previously-taken ["zag"] }
+(def note-unavailability
+  (fn [courses instructor-count registrants]
+    (let [out-of-instructors?
+          (= instructor-count
+             (count (filter (fn [course] (not (empty? course)))
+                            courses)))]
+      (map (fn [course]
+             (assoc course
+                    :unavailable? (or (:full? course)
+                                      (and out-of-instructors?
+                                           (:empty? course))
+                                      (and (:manager? registrants)
+                                           (not (:morning? course)))
+                                      (not (superset? (set (:previously-taken registrants))
+                                                      (set (:prerequisite course)))))))
+                    courses))))
+
+(def annotate
+  (fn [courses registrants instructor-count]
+    (-> courses
+      (answer-annotations registrants)
+      domain-annotations
+      (note-unavailability instructor-count registrants))))
