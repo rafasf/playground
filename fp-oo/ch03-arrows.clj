e47c2fbe9bbc51d7e0b22cfa982a7aec70d75b29
Rafael Ferreira <rafaelsf@gmail.com>
2013-06-13 13:46:04 -0500

    Some more math

--- fp-oo/ch03-arrows.clj
+++ fp-oo/ch03-arrows.clj
@@ -7,3 +7,13 @@
 (def inc-and-multiply
   (fn [v]
     (-> v first inc (* 3) list)))
+
+; Exercise 03
+(def by-2
+  (fn [n]
+    (-> n ((fn [n] (* 2 n))) inc)))
+
+; Exercise 04
+(def some-math
+  (fn []
+  (-> (+ 1 2) (* 3) (+ 4))))
