64824e4b9cc7c4d54f6b1dc788a9f8d6d6dfb096
Rafael Ferreira <rafaelsf@gmail.com>
2013-07-01 14:32:45 -0300

    Add algo.monads as dependency

---
+++ fp-oo/project.clj
@@ -0,0 +1,7 @@
+(defproject fp-oo "0.1.0-SNAPSHOT"
+  :description "fp-oo"
+  :url "no url yet"
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/algo.monads "0.1.4"]])
+
