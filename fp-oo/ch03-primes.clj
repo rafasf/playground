d915d19251c61b2b9f61e83880defed91228b749
Rafael Ferreira <rafaelsf@gmail.com>
2013-07-02 12:04:22 -0300

    More non primes

--- fp-oo/ch03-primes.clj
+++ fp-oo/ch03-primes.clj
@@ -1,3 +1,14 @@
+(use 'clojure.algo.monads)
+
+; Exercise 01
 (def multiples
   (fn [n]
     (range (* n 2) 101 n)))
+
+; Exercise 02
+(def nonprimes
+  (with-monad sequence-m
+    (domonad [i (range 2 11)
+              nonprime (multiples i)]
+             nonprime)))
+
