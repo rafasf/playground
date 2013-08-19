17a05dfc5bf15f40f54e6c39b573a5ae2cc6559c
Rafael Ferreira <rafaelsf@gmail.com>
2013-08-18 22:58:50 -0300

    Range implementation with LazySeq

---
+++ fp-oo/ch03-lazy.clj
@@ -0,0 +1,7 @@
+(def rrange
+  (fn [first past-end]
+    (new clojure.lang.LazySeq
+         (fn []
+           (if (= first past-end)
+             nil
+             (cons first (rrange (inc first) past-end)))))))
