aea7efae29b43b5d25647028ccd1d521301a695f
Rafael Ferreira <rafaelsf@gmail.com>
2013-08-19 13:39:28 -0300

    Map function using LazySeq

--- fp-oo/ch03-lazy.clj
+++ fp-oo/ch03-lazy.clj
@@ -5,3 +5,13 @@
            (if (= first past-end)
              nil
              (cons first (rrange (inc first) past-end)))))))
+
+; Exercise 01
+(def mmap
+  (fn [func a-seq]
+    (new clojure.lang.LazySeq
+         (fn []
+           (if (empty? a-seq)
+             nil
+             (cons (func (first a-seq))
+                   (mmap func (rest a-seq))))))))
