1f99748f52edfea00fe1ce65d1f5c7a72d8c947c
Rafael Ferreira <rafaelsf@gmail.com>
2013-08-24 20:41:59 -0300

    Reverse a sequence

--- fp-oo/ch03-pattern.clj
+++ fp-oo/ch03-pattern.clj
@@ -1,6 +1,12 @@
 (use 'patterned.sweet)
 
+; Exercise 01
 (defpatterned count-seq
   [so-far []] so-far
   [so-far [head & tail]] (count-seq (inc so-far) tail)
   [a-seq] (count-seq 0 a-seq))
+
+; Exercise 02
+(defpatterned pattern-reduce
+  [func so-far []] so-far
+  [func so-far [head & tail]] (pattern-reduce func (func so-far head) tail))
